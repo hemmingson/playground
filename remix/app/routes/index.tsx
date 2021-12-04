b22e300ab28dc1352bcad83bc6619fe9386e07c6
hemmingson <287969392@qq.com>
2021-12-04 22:59:47 +0800

    feat: update remix :cd:

--- remix/app/routes/index.tsx
+++ remix/app/routes/index.tsx
@@ -1,11 +1,17 @@
+import type { MetaFunction, LoaderFunction } from "remix";
 import { useLoaderData, json, Link } from "remix";
+
+type IndexData = {
+  resources: Array<{ name: string; url: string }>;
+  demos: Array<{ name: string; to: string }>;
+};
 
 // Loaders provide data to components and are only ever called on the server, so
 // you can connect to a database or run any server side code you want right next
 // to the component that renders it.
 // https://remix.run/api/conventions#loader
-export let loader = () => {
-  let data = {
+export let loader: LoaderFunction = () => {
+  let data: IndexData = {
     resources: [
       {
         name: "Remix Docs",
@@ -41,7 +47,7 @@ export let loader = () => {
 };
 
 // https://remix.run/api/conventions#meta
-export let meta = () => {
+export let meta: MetaFunction = () => {
   return {
     title: "Remix Starter",
     description: "Welcome to remix!"
@@ -50,7 +56,7 @@ export let meta = () => {
 
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
-  let data = useLoaderData();
+  let data = useLoaderData<IndexData>();
 
   return (
     <div className="remix__page">
